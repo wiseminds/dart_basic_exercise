7576d13aae91c2be62ad3f4e059eacb1205cc3be
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2022-08-09 14:26:36 +0100

    ---

--- basic_arithmetic_operations/bmi_calculator.dart
+++ basic_arithmetic_operations/bmi_calculator.dart
@@ -1,4 +1,3 @@
-
 // import 'test:test';
 
 /// this is a simple task to print the result of BMI calculation
@@ -6,12 +5,15 @@
 /// Body Mass Index is a simple calculation using a person’s height and weight. The formula is BMI = kg/m2 where kg is a person’s weight in kilograms and m2 is their height in metres squared.
 /// A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9. BMI applies to most adults 18-65 years.
 /// the result should read `You are {height}m tall and you weigh {weight}kg, your BMI is {bmi}`
-/// an example is if the user enters 2 as height and 60 as weight  `You are 2m tall and you weigh 60kg, your BMI is 15.0`
+/// an example is if the user enters 2 as height and 60 as weight  `You are 2m tall and you weight 60kg, your BMI is 15.0`
 ///
 /// to run code paste `dart basic_arithmetic_operations/bmi_calculator.dart ` in the terminal
 
 void main() {
-  
+  double height = 3.2;
+  double weight = 90.5;
 
-  print('PUT YOUR RESULT HERE');
+  double bmi = weight / (height * height);
+
+  print('You are ${height}m tall and you weight ${weight}kg, your BMI is $bmi');
 }
--- basic_arithmetic_operations/odd_even_numbers.dart
+++ basic_arithmetic_operations/odd_even_numbers.dart
@@ -1,13 +1,17 @@
-
-
 /// this is a simple task to print ODD or even depending on the number the user enters
 /// the result should read `number is an odd or even number`
 /// an example is if the user enters 2 the result should be `2 is an even number`
-/// 
+/// to know
+/// to check if a number is even, you can use the modulo operator. ` (number % 2) == 0`
 /// to run code paste `dart basic_arithmetic_operations/odd_even_numbers.dart ` in the terminal
 void main() {
-  
+  int number = 400;
 
-  print('PUT YOUR RESULT HERE');
- 
+  bool isEven = (number % 2) == 0;
+
+  if (isEven) {
+    print('$number is an even number');
+  } else {
+    print('$number is an odd number');
+  }
 }
